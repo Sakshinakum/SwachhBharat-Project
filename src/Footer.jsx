d33a798962f38d5c67738a0e60fa1da0d3d17b3f
Sakshinakum <sakshinakum06@gmail.com>
2025-08-26 15:07:33 +0530

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='Footer'>
+          <p>
+            <b>Copyright © 2025 </b>
+            <span className="brand">SwachhBharat. </span>
+            All rights reserved.
+          </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
